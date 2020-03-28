596b02397254c28917e9ea3d515d74267373045e
wenderDS <wender_ieu_lima@hotmail.com>
2020-03-28 13:28:07 -0400

    frontend do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import './global.css';
+import "./global.css";
 
-import Logon from './pages/Logon';
+import Routes from "./routes";
 
 function App() {
-  return (
-    <Logon />
-  );
+  return <Routes />;
 }
 
 export default App;
